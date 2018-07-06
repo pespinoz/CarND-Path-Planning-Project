ae55d903f411580ccd42e1f67908f9711033815a
Pablo Espinoza <pespinoz@email.arizona.edu>
2018-07-06 11:49:56 -0700

    Second Try: Makes the car go in circles; uses previous path as well

--- src/main.cpp
+++ src/main.cpp
@@ -251,13 +251,52 @@ int main() {
 
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
-            double dist_inc = 0.5;
-            for(int i = 0; i < 50; i++)
+            double pos_x;
+            double pos_y;
+            double angle;
+            double pos_x2;
+            double pos_y2;
+
+            int path_size = previous_path_x.size();
+            std::cout << path_size << std::endl;
+
+            for(int i = 0; i < path_size; i++)
             {
-                next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
-                next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
+                next_x_vals.push_back(previous_path_x[i]);
+                next_y_vals.push_back(previous_path_y[i]);
             }
 
+            if(path_size == 0)
+            {
+                pos_x = car_x;
+                pos_y = car_y;
+                angle = deg2rad(car_yaw);
+            }
+            else
+            {
+                pos_x = previous_path_x[path_size-1];
+                pos_y = previous_path_y[path_size-1];
+
+                pos_x2 = previous_path_x[path_size-2];
+                pos_y2 = previous_path_y[path_size-2];
+                angle = atan2(pos_y-pos_y2,pos_x-pos_x2);
+            }
+
+            double dist_inc = 0.5;
+            for(int i = 0; i < 50-path_size; i++)
+            {
+                next_x_vals.push_back(pos_x+(dist_inc)*cos(angle+(i+1)*(pi()/100)));
+                next_y_vals.push_back(pos_y+(dist_inc)*sin(angle+(i+1)*(pi()/100)));
+                pos_x += (dist_inc)*cos(angle+(i+1)*(pi()/100));
+                pos_y += (dist_inc)*sin(angle+(i+1)*(pi()/100));
+            }
+
+
+            msgJson["next_x"] = next_x_vals;
+            msgJson["next_y"] = next_y_vals;
+
+
+            // Continue
 
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
