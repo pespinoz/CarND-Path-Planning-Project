7dbf56c026244bf04bcd83399feaf2ef1f88e5e2
Pablo Espinoza <pespinoz@email.arizona.edu>
2018-07-06 20:06:38 -0700

    Third try: Car stays in its lane by using Frenet coords

--- src/main.cpp
+++ src/main.cpp
@@ -251,49 +251,20 @@ int main() {
 
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
-            double pos_x;
-            double pos_y;
-            double angle;
-            double pos_x2;
-            double pos_y2;
-
-            int path_size = previous_path_x.size();
-            std::cout << path_size << std::endl;
-
-            for(int i = 0; i < path_size; i++)
-            {
-                next_x_vals.push_back(previous_path_x[i]);
-                next_y_vals.push_back(previous_path_y[i]);
-            }
-
-            if(path_size == 0)
-            {
-                pos_x = car_x;
-                pos_y = car_y;
-                angle = deg2rad(car_yaw);
-            }
-            else
-            {
-                pos_x = previous_path_x[path_size-1];
-                pos_y = previous_path_y[path_size-1];
-
-                pos_x2 = previous_path_x[path_size-2];
-                pos_y2 = previous_path_y[path_size-2];
-                angle = atan2(pos_y-pos_y2,pos_x-pos_x2);
-            }
-
             double dist_inc = 0.5;
-            for(int i = 0; i < 50-path_size; i++)
+            double next_s;
+            double next_d;
+            vector<double> xy;
+
+            for(int i = 0; i < 50; i++)
             {
-                next_x_vals.push_back(pos_x+(dist_inc)*cos(angle+(i+1)*(pi()/100)));
-                next_y_vals.push_back(pos_y+(dist_inc)*sin(angle+(i+1)*(pi()/100)));
-                pos_x += (dist_inc)*cos(angle+(i+1)*(pi()/100));
-                pos_y += (dist_inc)*sin(angle+(i+1)*(pi()/100));
+                next_s = car_s + (i+1)*dist_inc; // Using 'i' the car'd stay in its initial position
+                next_d = 6;
+                xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+                next_x_vals.push_back(xy[0]);
+                next_y_vals.push_back(xy[1]);
             }
-
-
-            msgJson["next_x"] = next_x_vals;
-            msgJson["next_y"] = next_y_vals;
 
 
             // Continue
